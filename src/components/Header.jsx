0163453a8ab87240166a360883a32e5d60968c24
Dylan Losada <dylanlosada67@gmail.com>
2021-01-20 22:53:49 -0300

    Upload second challenge

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Logo from './HeaderComponents/Logo';
+import NavBar from './HeaderComponents/NavBar';
+
+const Header = () => {
+
+    return ( 
+        <header id = 'header'>
+            <Logo /> 
+            <NavBar /> 
+        </header>
+     );
+
+}
+export default Header;
---
+++ src/components/HeaderComponents/Logo.jsx
@@ -0,0 +1,7 @@
+const Logo = () => ( 
+        <div>
+           <div id = 'logoTitle'></div>
+        </div>
+);
+
+export default Logo;
