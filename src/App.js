c03ff258090dae41a9d4ee0b4bd1de31aa2f07bc
Dylan Losada <dylanlosada67@gmail.com>
2021-01-27 12:48:19 -0300

    Update Challenge 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Header from './components/header/Header'
+import Header from './components/header/Header';
+import Main from './components/main/Main';
 import './styles/App.scss';
 
 function App() {
   return (
     <>
       <Header />
+      <Main 
+        message = 'Voy a ser el Main de la página de Mercado Libre!!!'
+      />
     </>
   );
 }
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Main = ({message}) => ( 
+    <>
+        <h1>{message}</h1>
+    </>
+ );
+
+ 
+export default Main;
